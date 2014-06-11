be2fd6c4312d3870c2920366a4baa71ea0bcdc6c
Ting-Yen Lai <banacorn@gmail.com>
2014-06-12 02:02:29 +0800

    get angular DI working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,20 @@
 'use strict';
 
 angular
-  .module('paperclipApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('paperclipApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'btford.socket-io'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('paperclipApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .factory('socket', function(socketFactory) {
+
+        var socket = window.io('http://localhost:4000');
+
+        var options = {
+            ioSocket: socket
+        };
+        return socketFactory(options);
+    })
+    .controller('MainCtrl', function($scope, socket) {
+
+        socket.emit('hey', 'hi');
+
+        $scope.items = ['a', 'b'];
+
+        $scope.addTodo = function() {
+            $scope.items.push($scope.todoText);
+        };
+    });
