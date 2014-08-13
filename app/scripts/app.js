07e0863cea5eafd43005ed3b8ccd80c686674512
Ting-Yen Lai <banacorn@gmail.com>
2014-08-13 22:13:11 +0800

    header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,21 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name paperclipApp
- * @description
- * # paperclipApp
- *
- * Main module of the application.
- */
 angular
-  .module('paperclipApp', [
-    'ngResource',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('paperclipApp', [
+        'ngResource',
+        'ngRoute'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
