fbec9f9d3ad004a4b15dd7f12280914fa446872a
Ting-Yen Lai <banacorn@gmail.com>
2014-08-13 23:56:09 +0800

    testController.connect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,23 @@
 'use strict';
 
+function Socket(socketFactory) {
+
+    var socket = null;
+
+    this.connect = function() {
+        if (socket !== null) {
+            throw 'socket already connected and created!';
+        } else {
+            socket = socketFactory({
+                ioSocket: window.io.connect('http://localhost:3000')
+            });
+        }
+    };
+}
+
 angular
     .module('paperclipApp', ['btford.socket-io'])
-    .factory('socket', function(socketFactory) {
-        return socketFactory({
-            ioSocket: window.io.connect('http://localhost:3000')
-        });
-    })
-    .controller('socketController', function(socket) {
-        socket.on('connect', function() {
-            console.log('connected!!');
-        });
-
+    .service('socket', ['socketFactory', Socket])
+    .controller('testController', function(socket) {
+        this.connect = socket.connect;
     });
